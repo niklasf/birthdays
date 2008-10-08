3609c43aadb6b5d15ba89045a62edbf5e7ce2885
Maarten van Grootel <maartenvg@109716.no-reply.drupal.org>
2008-10-08 12:08:31 +0000

    Added preprocessed template for blocks, to help customizing. Requested in #317958, #290322, #193378 and others

---
+++ birthdays-block.tpl.php
@@ -0,0 +1,44 @@
+<?php
+// $Id$
+
+/**
+ * @file birthdays-block.tpl.php
+ * Display the users that are having their birthday, as requested by one of the
+ * blocks.
+ *
+ * Available variables:
+ * - $no_birthdays:   Boolean indicating whether there birthdays.
+ * - $show_starsigns: Boolean indication whether to show the star sign.
+ * - $amount:         Number of days or users to be shown in the block (depends
+ *                    on the type of block).
+ * - $block_type:     Which block is shown? Either 'by_days' or 'by_birthdays'.
+ * - $more:           Themed link to the Birthdays page.
+ * - $birthdays:      Associative array with the following data:
+ *   - 'username':      Themed username with link to account.
+ *   - 'age':           Age the user will be after his/her birthday, shown
+ *                      depending on the settings of the module or the user's
+ *                      preference.
+ *   - 'date':          Month and day of birth of the user, formatted according
+ *                      the system's "short date format".
+ *   - 'account':       Entire account, for user pictures and other ways to
+ *                      improve the birthdays block.
+ *   - 'starsign':      Themed star sign.
+ *
+ * @see template_preprocess_forum_icon()
+ * @see theme_forum_icon()
+ */
+?>
+<?php if ($no_birthdays): ?>
+  <p><?php print t('There are no birthdays soon.'); ?></p>
+<?php else: ?>
+  <table>
+    <?php foreach($birthdays as $birthday): ?>
+    <tr>
+      <td><?php print $birthday['username'] ?>&nbsp;<small>(<?php print $birthday['age'] ?>)</small></td>
+      <td><?php print $birthday['date'] ?></td>
+    </tr>
+    <?php endforeach; ?>
+  </table>
+<?php endif; ?>
+
+<? print $more; ?>

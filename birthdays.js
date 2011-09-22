1e5de6d1493778903f2383eb70ef5f726df9d107
Niklas Fiekas <niklas.fiekas@googlemail.com>
2011-09-22 02:17:14 +0200

    #1281406: Make datepicker respect hide_year

--- birthdays.js
+++ birthdays.js
@@ -2,15 +2,22 @@
 
 Drupal.behaviors.birthdays = {
   attach: function (context) {
-    $('.birthdays-datepicker', context).datepicker({
+    var settings = {
       dateFormat: 'yy/mm/dd',
       autoSize: true,
       changeMonth: true,
       changeYear: true,
       maxDate: '+0',
       minDate: '-115y',
-      yearRange: '-115:+0'
-    });
+      yearRange: '-115:+0',
+      constrainInput: false
+    };
+
+    var noYearSettings = jQuery.extend({}, settings);
+    noYearSettings.dateFormat = 'mm/dd';
+
+    $('.birthdays-datepicker', context).datepicker(settings);
+    $('.birthdays-datepicker-noyear', context).datepicker(noYearSettings);
   }
 };
 
